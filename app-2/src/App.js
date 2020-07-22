07557dbcff1598ec5a4c2f817310eb9a5792767a
800mSanchez <alejandrosanchez9494@gmail.com>
2020-07-22 15:53:27 -0500

    App 2 Complete

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      runners: [ "David Rudisha", "Donovan Brazier", "Lopez Lomong"]
+    }
+  }
+
+  render(){
+    let runnersToDisplay = this.state.runners.map((element,index) => {
+  return <h2 key={index}>{element}</h2>
+    })
+    return <div className="App">{runnersToDisplay}</div>
+}
+  
 }
 
 export default App;
